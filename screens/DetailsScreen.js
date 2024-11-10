c9139991d83a6c70827405c6e570ff89999bab76
malathi <malathisankar17@gmail.com>
2024-11-10 23:41:13 +0530

    navigate_twoscreen

---
+++ screens/DetailsScreen.js
@@ -0,0 +1,23 @@
+// screens/DetailsScreen.js
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+export default function DetailsScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go back to Home"
+        onPress={() => navigation.goBack()}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ screens/HomeScreen.js
@@ -0,0 +1,23 @@
+// screens/HomeScreen.js
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
